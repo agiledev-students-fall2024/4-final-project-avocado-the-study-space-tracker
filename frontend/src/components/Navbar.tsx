f7193b38d2932e865cb0617e174cd9e08632eddd
rzh4321 <rzh4321@gmail.com>
2024-10-24 13:56:14 -0400

    linter

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { Button } from "./ui/button";
 
 export default function Navbar() {
-
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -9,15 +8,11 @@ export default function Navbar() {
 
         {/* Logo in the middle */}
         <div className="w-1/3 flex justify-center">
-          <div className="text-white text-2xl font-bold">
-            LOGO
-          </div>
+          <div className="text-white text-2xl font-bold">LOGO</div>
         </div>
 
         <div className="w-1/3 flex justify-end">
-          <Button variant={"destructive"}>
-            Log Out
-          </Button>
+          <Button variant={"destructive"}>Log Out</Button>
         </div>
       </div>
     </nav>
